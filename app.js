ed10e4d3ba280e56889f2f903e6a0c8efb331346
posh101 <paulorife@gmail.com>
2024-07-03 13:06:57 -0700

    new parameter

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const https = require('https')
 
 const app = express();
-app.use(express.json());
 app.set('trust proxy', true);
+app.use(express.json());
 
 const apiKey = "38c3094491af463d02763f0e5c714782";
 
 const locationData = {
     country: "Nigeria",
     city: "Lagos",
-    name: "George"
+    name: "Mark"
 }
 
 const api = "https://api.openweathermap.org/data/2.5/weather?q=" + locationData.city +" &mode=json&units=metric&appid=" + apiKey;
@@ -22,14 +22,10 @@ app.get("/api/hello",  (req, res) => {
         response.on ("data", function(data) {
           const weatherData = JSON.parse(data)
           const temperature = weatherData.main.temp
-      // message =  
-      //    res.write(`{ip:${ip} 
-      //   "Location": ${locationData.city} 
-      //   "greeting": Hello, ${locationData.name}!, the temperature  is ${temperature} degree celcius in ${locationData.city}}`)
-          res.send({
-            ip: ip,
+            res.json({
+            "Client_ip": ip,
             "Location": locationData.city,
-            "greeting": `Hello, ${locationData.name}!, the temperature is ${temperature} degrees celcius in ${locationData.city}`,
+            "greeting": `Hello, ${locationData.name}!, the temperature is ${temperature} degree celcius in ${locationData.city}`,
           })
         })
      })
