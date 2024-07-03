92f64b2d64593c98f3cd6d013d05fe55dc691fda
posh101 <paulorife@gmail.com>
2024-07-03 10:29:13 -0700

    add parameter

--- app.js
+++ app.js
@@ -8,28 +8,32 @@ app.set('trust proxy', true);
 const apiKey = "38c3094491af463d02763f0e5c714782";
 
 const locationData = {
-    country: "Nigeria ",
+    country: "Nigeria",
     city: "Lagos",
-    name: "James"
+    name: "George"
 }
 
 const api = "https://api.openweathermap.org/data/2.5/weather?q=" + locationData.city +" &mode=json&units=metric&appid=" + apiKey;
  
-app.get("/",  (req, res) => {
-   let message = req.query.message 
+app.get("/api/hello",  (req, res) => {
+   let { visitor_name } = req.query;
     const ip = req.ip;
      https.get(api, function(response) {
         response.on ("data", function(data) {
           const weatherData = JSON.parse(data)
           const temperature = weatherData.main.temp
-      message =  
-         res.write(`{ip:${ip} 
-        "Location": ${locationData.city} 
-        "greeting": Hello, ${locationData.name}!, the temperature  is ${temperature} degree celcius in ${locationData.city}}`)
-          res.send()
+      // message =  
+      //    res.write(`{ip:${ip} 
+      //   "Location": ${locationData.city} 
+      //   "greeting": Hello, ${locationData.name}!, the temperature  is ${temperature} degree celcius in ${locationData.city}}`)
+          res.send({
+            ip: ip,
+            "Location": locationData.city,
+            "greeting": `Hello, ${locationData.name}!, the temperature is ${temperature} degrees celcius in ${locationData.city}`,
+          })
         })
      })
-    
+     
 });
 
 app.listen(4000, () => {
